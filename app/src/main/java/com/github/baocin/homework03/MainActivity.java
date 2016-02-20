f08af71251c9316c73a8056ea6b013537a617b96
Michael Pedersen <steele.pedersen@gmail.com>
2016-02-20 04:30:16 -0500

    Added timer to auto start quiz after 8 seconds.

--- app/src/main/java/com/github/baocin/homework03/MainActivity.java
+++ app/src/main/java/com/github/baocin/homework03/MainActivity.java
@@ -1,13 +1,53 @@
 package com.github.baocin.homework03;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Timer autoStartQuiz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imgSplash = (ImageView) findViewById(R.id.imgSplash);
+        imgSplash.setImageResource(R.drawable.splash_screen);
+
+        Button btnStartQuiz = (Button) findViewById(R.id.btnStartQuiz);
+        btnStartQuiz.setOnClickListener(this);
+
+        final TimerTask startQuiz = new TimerTask() {
+            @Override
+            public void run() {
+                startQuiz();
+            }
+
+        };
+        autoStartQuiz = new Timer("autoStart");
+        autoStartQuiz.schedule(startQuiz, 8000);
+
+
+    }
+
+    public void startQuiz(){
+        Intent quiz = new Intent(getBaseContext(), Welcome.class);
+        startActivity(quiz);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btnStartQuiz:
+                startQuiz();
+                break;
+        }
     }
 }
